5893d0096f0eb151f9ffc220cedbbef2dae7fe46
coli <kkwoo001021@naver.com>
2025-12-29 17:11:01 +0900

    feat: RequestLine 구조 선언

---
+++ src/main/java/webserver/http/HttpRequestLine.java
@@ -0,0 +1,14 @@
+package webserver.http;
+
+public class HttpRequestLine {
+
+    private final HttpMethod method;
+    private final HttpVersion version;
+    private final RequestUrl requestUrl;
+
+    public HttpRequestLine(HttpMethod method, HttpVersion version, RequestUrl requestUrl) {
+        this.method = method;
+        this.version = version;
+        this.requestUrl = requestUrl;
+    }
+}
---
+++ src/main/java/webserver/http/RequestUrl.java
@@ -0,0 +1,10 @@
+package webserver.http;
+
+public class RequestUrl {
+
+    private final String url;
+
+    public RequestUrl(String url) {
+        this.url = url;
+    }
+}
