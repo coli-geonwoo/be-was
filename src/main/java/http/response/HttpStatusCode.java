302ae89acf408854789a9aeaa58a5596393ae286
coli <kkwoo001021@naver.com>
2025-12-30 23:57:50 +0900

    feat: statusCode 구현

---
+++ src/main/java/http/response/HttpStatusCode.java
@@ -0,0 +1,28 @@
+package http.response;
+
+public enum HttpStatusCode {
+
+    OK_200(200, "OK"),
+    CREATED_201(201, "Created"),
+    NOT_FOUND_404(404, "Not Found"),
+    BAD_REQUEST_400(400, "Bad Request"),
+    UNAUTHORIZED_401(401, "Unauthorized"),
+    INTERNAL_SERVER_ERROR_500(500, "Internal Server Error"),
+    ;
+
+    private final int code;
+    private final String message;
+
+    HttpStatusCode(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
