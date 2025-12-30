fc77d279280e2ad65964d92dfc54a6c1bd0edf72
coli <kkwoo001021@naver.com>
2025-12-30 23:58:00 +0900

    feat: responseBody 구현

---
+++ src/main/java/http/response/HttpResponseBody.java
@@ -0,0 +1,14 @@
+package http.response;
+
+public class HttpResponseBody {
+
+    private final byte[] body;
+
+    public HttpResponseBody(byte[] body) {
+        this.body = body;
+    }
+
+    public byte[] getBody() {
+        return body;
+    }
+}
