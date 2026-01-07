d16a9153c5ab61784b594b8dac6dd7c5d5e3f6fd
coli <kkwoo001021@naver.com>
2026-01-07 14:03:46 +0900

    feat: HandlerKey 구현

---
+++ src/main/java/webserver/handler/HandlerKey.java
@@ -0,0 +1,21 @@
+package webserver.handler;
+
+import http.request.HttpMethod;
+import java.util.Arrays;
+
+public class HandlerKey {
+
+    private final HttpMethod method;
+    private final String[] paths;
+
+    public HandlerKey(HttpMethod method, String[] paths) {
+        this.method = method;
+        this.paths = paths;
+    }
+
+    public boolean matches(HttpMethod method, String path) {
+        return method.equals(this.method)
+                && Arrays.asList(paths)
+                .contains(path);
+    }
+}
