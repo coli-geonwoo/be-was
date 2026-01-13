eb005be0712c0860ea94aaacfba295255ca52d15
coli <kkwoo001021@naver.com>
2026-01-13 12:10:00 +0900

    chore: FileUploader 구현

---
+++ src/main/java/application/util/FileUploader.java
@@ -0,0 +1,25 @@
+package application.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class FileUploader {
+
+    public FileUploader() {
+    }
+
+    public void imageUpload(InputStream inputStream, String path) {
+        try {
+            Path uploadPath = Paths.get(path);
+            if(!Files.exists(uploadPath)) {
+                throw new RuntimeException("File does not exist");
+            }
+            Files.copy(inputStream, uploadPath);
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to upload image", e);
+        }
+    }
+}
