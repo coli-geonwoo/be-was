7b2e68fbd7e2d3198296f833848de2bfae1c5b63
coli <kkwoo001021@naver.com>
2026-01-15 02:38:47 +0900

    refactor: 응답 dto 형식 변경

--- src/main/java/application/dto/response/ArticleResponse.java
+++ src/main/java/application/dto/response/ArticleResponse.java
@@ -1,16 +1,33 @@
 package application.dto.response;
 
+import application.model.Article;
+import application.model.ArticleImage;
+import application.model.User;
 import java.util.List;
 
 public record ArticleResponse(
+        int total,
         String title,
         String content,
         String nickname,
         String imageUrl,
         List<String> images,
-        int likes,
+        long likes,
         CommentResponse comments
 
 ) {
+
+    public ArticleResponse(int total, Article article, User user, List<ArticleImage> images, long likes, CommentResponse commentResponse) {
+        this(
+                total,
+                article.getTitle(),
+                article.getContent(),
+                user.getName(),
+                user.getImageUrl(),
+                images.stream().map(ArticleImage::getImageUrl).toList(),
+                likes,
+                commentResponse
+        );
+    }
 
 }
